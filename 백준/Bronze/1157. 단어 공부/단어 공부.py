451ccecac7812920bfea196f6a7a9db73b18f555
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-02-19 18:29:49 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 33212 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,13 @@
+words = input().upper()
+unique_words = list(set(words))  # 입력받은 문자열에서 중복값을 제거
+
+cnt_list = []
+for x in unique_words :
+    cnt = words.count(x)
+    cnt_list.append(cnt)  # count 숫자를 리스트에 append
+
+if cnt_list.count(max(cnt_list)) > 1 :  # count 숫자 최대값이 중복되면
+    print('?')
+else :
+    max_index = cnt_list.index(max(cnt_list))  # count 숫자 최대값 인덱스(위치)
+    print(unique_words[max_index])
