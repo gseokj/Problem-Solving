1dd386c01b177570c6feee0d17598e98809235af
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-02-19 18:36:19 +0900

    Update 상수.py

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,5 +1,5 @@
 A, B = input().split()
-if (int(A[::-1])>int(B[::-1])):
+if (int(A[::-1])>int(B[::-1])): # 문자열로 받아온 정수를 슬라이싱을 사용한 후 인트형으로 비교연산자를 사용한다.
     print(int(A[::-1]))
 else :
     print(int(B[::-1]))
