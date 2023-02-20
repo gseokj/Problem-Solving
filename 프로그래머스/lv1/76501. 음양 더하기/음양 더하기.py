0da4fe55fbde7be783306761cb05e29023558006
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-02-20 15:17:29 +0900

    Update 음양 더하기.py

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -1,6 +1,2 @@
 def solution(absolutes, signs):
-    answer = 123456789
-    for i in range(len(absolutes)):
-        if signs[i]==False :
-            absolutes[i]=-absolutes[i]
-    return sum([-absolutes[i] if signs[i]=='false' else absolutes[i] for i in range(len(absolutes))])
+    return sum([-absolutes[i] if signs[i]==False else absolutes[i] for i in range(len(absolutes))])
