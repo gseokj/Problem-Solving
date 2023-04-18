3a063095097e1f416a7336a192fb6ea786c5dfbf
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-04-18 22:25:47 +0900

    [Silver V] Title: 다리 놓기, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,12 @@
+def dp_b (n,m) :
+    dp = [[1]*(m+1) for _ in range(m+1)]
+    for i in range(2,m+1):
+        dp[1][i]=i
+    for i in range(2,n+1):
+        for j in range(i+1,m+1):
+            dp[i][j] = dp[i-1][j-1] + dp[i][j-1]
+    return dp[n][m]
+
+for _ in range(int(input())):
+    east, west =  map(int, input().split())
+    print(dp_b(east,west))
