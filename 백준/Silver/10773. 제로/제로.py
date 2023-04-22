002376c4f0c8b15cf5e4d6703c78039ed97b0257
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-04-22 15:13:27 +0900

    [Silver IV] Title: 제로, Time: 3848 ms, Memory: 32036 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,12 +1,9 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+stack = []
+for _ in range(int(input())):
+    comm = int(input())
+    if comm == 0 :
+        if stack :
+            stack.pop()
+    else :
+        stack.append(comm)
+print(sum(stack))
