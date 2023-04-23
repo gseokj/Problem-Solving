c6b72b1e7c1dc4c7b08fdf42b62f310abb85ab13
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-04-23 12:59:34 +0900

    [Silver IV] Title: 베스트셀러, Time: 64 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.py
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -1,12 +1,12 @@
 from collections import Counter
-T = int(input())
-lis = []
-best_seller=[]
-for i in range(T):
-    lis.append(input())
+
+lis = [input() for i in range(int(input()))]
 comm = Counter(lis).most_common()
+best_seller=[]
+
 for i in comm :
     if comm[0][1]==i[1] :
         best_seller.append(i[0])
 best_seller.sort()
+
 print(best_seller[0])
