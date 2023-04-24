5da4afbba0aba0f589513efa7d4b5787161ce9ee
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-04-24 19:34:14 +0900

    [Silver IV] Title: 보물, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,12 +1,13 @@
 n = int(input())
-
-a_list = list(map(int, input().split()))
-b_list = list(map(int, input().split()))
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
 
 s = 0
 for i in range(n):
-    s += min(a_list) * max(b_list)
-    a_list.pop(a_list.index(min(a_list)))
-    b_list.pop(b_list.index(max(b_list)))
+    a_min = min(a)
+    b_max = max(b)
+    s += a_min * b_max
+    a.remove(a_min)
+    b.remove(b_max)
 
 print(s)
