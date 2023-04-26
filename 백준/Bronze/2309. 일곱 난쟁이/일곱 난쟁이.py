6622e6c605d64692c6bbb45329af38e99e3c75a4
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-04-27 01:10:18 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,16 @@
+array = []
+for i in range(9):
+    array.append(int(input()))
+
+array.sort()
+
+sum_ = sum(array)
+for i in range(len(array)):
+    for j in range(i + 1, len(array)):
+        if sum_ - array[i] - array[j] == 100:
+            for k in range(len(array)):
+                if k == i or k == j:
+                    pass
+                else:
+                    print(array[k])
+            exit()
