06f32a17977c21e173252ef7339f838d919a9edf
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-05-03 20:50:17 +0900

    [Silver V] Title: 집합, Time: 4904 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+m = int(input())
+S = set()
+for _ in range(m):
+    temp = input().split()
+    if len(temp) == 1:
+        if temp[0] == "all":
+            S = set([i for i in range(1, 21)])
+        else:
+            S = set()
+    
+    else:
+        func, x = temp[0], temp[1]
+        x = int(x)
+
+        if func == "add":
+            S.add(x)
+        elif func == "remove":
+            S.discard(x)
+        elif func == "check":
+            print(1 if x in S else 0)
+        elif func == "toggle":
+            if x in S:
+                S.discard(x)
+            else:
+                S.add(x)
