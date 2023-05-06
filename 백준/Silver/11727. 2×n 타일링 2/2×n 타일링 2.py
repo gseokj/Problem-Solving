d727029d63ed84b41029d9619bf75f0ea0d56ed3
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-05-06 20:17:14 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 48 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+def dp_test(n):
+    dp = [0] * 1001
+    dp[1] = 1
+    dp[2] = 3
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]*2
+    return dp[n]
+
+
+T = int(input())
+print(dp_test(T)%10007)
