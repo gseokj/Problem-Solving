d4b5a935e237973f623d34796edaf48ea6e5c7fa
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-05-08 22:56:47 +0900

    [Silver V] Title: 피보나치 수 4, Time: 44 ms, Memory: 34916 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -0,0 +1,12 @@
+def dp_test(n):
+    dp = [0] * 10001
+    dp[1] = 1
+
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return dp[n]
+
+
+n = int(input())
+print(dp_test(n))
+
