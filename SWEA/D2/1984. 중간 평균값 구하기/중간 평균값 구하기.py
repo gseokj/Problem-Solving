aec6a6a44958d713ed8eafedc537fdf202d5c5da
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-05-09 17:59:14 +0900

    [D2] Title: 중간 평균값 구하기, Time: 146 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.py
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.py
@@ -1,6 +1,3 @@
 for x in range(1,int(input())+1):
-    arr = list(map(int,input().split()))
-    t_arr = sorted(arr)[1:-1]
-    total = sum(t_arr)
-    l = len(t_arr)
-    print(f"#{x} {round(total/l)}")
+    arr = sorted(list(map(int,input().split())))[1:-1]
+    print(f"#{x} {round(sum(arr)/len(arr))}")
