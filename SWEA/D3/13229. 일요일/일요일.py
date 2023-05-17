2b9f8a91ddf9f81d8ee2ee283d718b011447d5a5
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-05-17 22:35:42 +0900

    [D3] Title: 일요일, Time: 154 ms, Memory: 58,256 KB -BaekjoonHub

--- SWEA/D3/13229. 일요일/일요일.py
+++ SWEA/D3/13229. 일요일/일요일.py
@@ -1,3 +1,3 @@
-for x in range(1, int(input()) + 1):
-    arr={"SAT":1,"FRI":2,"THU":3,"WED":4,"TUE":5,"MON":6,"SUN":7}
-    print(f"#{x} {arr[input()]}")
+for x in range(1,int(input())+1):
+    arr=["SAT","FRI","THU","WED","TUE","MON","SUN"]
+    print(f"#{x} {arr.index(input())+1}")
