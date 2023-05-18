87f3518605547d111dc962f5f5c2a73d04755305
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-05-19 00:09:33 +0900

    [D3] Title: 홀수일까 짝수일까, Time: 177 ms, Memory: 58,764 KB -BaekjoonHub

--- SWEA/D3/5549. 홀수일까 짝수일까/홀수일까 짝수일까.py
+++ SWEA/D3/5549. 홀수일까 짝수일까/홀수일까 짝수일까.py
@@ -1,2 +1,1 @@
-for x in range(int(input())):
-    print(f"#{x+1}","Odd" if int(input())%2 else "Even")
+print('\n'.join([f"#{x+1} {'Odd' if int(input()) % 2 else 'Even'}" for x in range(int(input()))]))
