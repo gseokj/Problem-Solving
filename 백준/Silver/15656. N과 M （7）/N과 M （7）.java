321cdcf9506898f7194c68f2fca9ed8e763c72b6
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-08-04 09:07:51 +0900

    [Silver III] Title: N과 M (7), Time: 644 ms, Memory: 258812 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] P;
+	static int[] Nums;
+	static int N;
+	static int R;
+	static boolean[] Visited;
+	static StringBuilder sb;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		P = new int[N];
+		Visited = new boolean[N];
+		Nums = new int[R];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			P[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(P);
+		perm(0);
+		System.out.println(sb.toString());
+	}
+	static void perm(int cnt) {
+		if (cnt==R) {
+			for (int n : Nums) {
+				sb.append(n+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			Visited[i] =true;
+			Nums[cnt] = P[i];
+			perm(cnt+1);
+			Nums[cnt] = 0;
+			Visited[i] =false;
+		}
+	}
+}
