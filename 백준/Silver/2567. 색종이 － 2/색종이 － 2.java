1e4a3f709aa5433b688cfa2096f3b4f58f71ff5e
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-08-13 23:57:28 +0900

    [Silver IV] Title: 색종이 - 2, Time: 80 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
@@ -2,57 +2,45 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 	static int N;
-	static int[][] selected;
-	static int res;
-	static int[] dr = {-1,0,1,0};
-	static int[] dc = {0,1,0,-1};
+	static int[][] map;
+	static int[] dr= {1,-1,0,0};
+	static int[] dc= {0,0,1,-1};
 	public static void main(String[] args) throws IOException {
 		//System.setIn(new FileInputStream("src/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		selected = new int[101][101];
-		for (int x = 0; x < N; x++) {
+		map = new int[101][101];
+		for (int i = 0; i < N; i++) {
+			
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int r = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			for (int i = r; i < r + 10; i++) {
-				for (int j = c; j < c + 10; j++) {
-						selected[i][j] = 1;
+			for (int j = r; j < r+10; j++) {
+				for (int k = c; k < c+10; k++) {
+					map[j][k] = 1;
 				}
-
 			}
-			
-
 		}
-		res=0;
+		int res =0;
 		for (int i = 1; i <= 100; i++) {
 			for (int j = 1; j <= 100; j++) {
-				if(selected[i][j] ==1) {
-					bfs(i,j);
+				if (map[i][j] == 1) {
+					for (int k = 0; k < 4; k++) {
+						int r = dr[k]+i;
+						int c = dc[k]+j;
+						if(map[r][c] == 0) res++; 
+					}
+					
 				}
 			}
 		}
-        System.out.println(res);
-    }
-	
-	private static void bfs(int r,int c) {
+		System.out.println(res);
 		
-			for (int d = 0; d < 4; d++) {
-				int nr = r+dr[d];
-				int nc = c+dc[d];
-				if(selected[nr][nc] ==0) res++;
-				
-				
-			}
-		}
-	
+	}
 
 
 }
