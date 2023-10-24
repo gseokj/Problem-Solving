a4d9f4bd8be86243aaab541cd4734af6d0010335
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2023-10-24 21:07:19 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 652 ms, Memory: 90456 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.java
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.java
@@ -8,6 +8,7 @@ public class Main {
 	static int min;
 	static int max;
 	static int N,M,K;
+	static StringBuilder sb;
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new FileInputStream("src/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,6 +18,7 @@ public class Main {
 		tree = new int[N*4];
 		treeR = new int[N*4];
 		nums = new int[N+1];
+		sb = new StringBuilder();
 		for (int i = 1; i < N+1; i++) {
 			nums[i] = Integer.parseInt(br.readLine());
 		}
@@ -28,11 +30,10 @@ public class Main {
 			int b = Integer.parseInt(st.nextToken());
 			min = Integer.MAX_VALUE;
 			max = 0;
-			int r1 =find(1,N,1,a,b);
-			int r2=findR(1,N,1,a,b);
-			System.out.println(r1+" "+r2);
-
+			sb.append(find(1,N,1,a,b)+" ");
+			sb.append(findR(1,N,1,a,b)+"\n");
 		}
+		System.out.println(sb.toString());
 	}
 	private static int init(int start, int end, int node) {
 		if(start==end) return tree[node] = nums[start];
