662330ac3deeb80a9fff9e61c3d64e6922ce420f
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2023-10-25 10:15:30 +0900

    [Gold I] Title: 구간 합 구하기, Time: 748 ms, Memory: 178560 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,25 +1,19 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static long[] nums,tree;
+	static long[] tree;
+	static long[] nums;
+	static int N,M,K;
 	public static void main(String[] args) throws Exception {
-		//System.setIn(new FileInputStream("src/input.txt"));
+	//	System.setIn(new FileInputStream("src/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken()); // 총 개수
-		int M = Integer.parseInt(st.nextToken()); // 변경 횟수
-		int K = Integer.parseInt(st.nextToken()); // 합을 구하는 횟수
-		nums = new long[N+1];
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 		tree = new long[4*N];
+		nums = new long[N+1];
 		for (int i = 1; i < N+1; i++) {
 			nums[i] = Long.parseLong(br.readLine());
 		}
@@ -29,35 +23,34 @@ public class Main {
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			long c = Long.parseLong(st.nextToken());
-			
 			if(a==1) {
-				long dif = c -nums[b];
+				long dif = c-nums[b];
 				nums[b] = c;
 				update(1,N,1,b,dif);
 			}else {
 				System.out.println(sum(1,N,1,b,(int)c));
 			}
+			
 		}
-		
-		
-		}
-	static long init(int start, int end, int node) {
-		if(start==end) return tree[node] = nums[start];
-		int mid=(start+end)/2;
-		return tree[node] = init(start,mid,node*2)+init(mid+1,end,node*2+1);
 	}
-	static long sum(int start, int end, int node, int left, int right) {
-		if(left>end||right<start) return 0;
-		if(left<=start&&end<=right) return tree[node];
+	private static long sum (int start, int end, int node, int left, int right) {
+		if(left>end || right <start) return 0;
+		if(left<=start && end <=right) return tree[node];
 		int mid = (start+end)/2;
 		return sum(start,mid,node*2,left,right)+sum(mid+1,end,node*2+1,left,right);
 	}
-	static void update(int start, int end, int node, int index, long dif) {
-		if(index<start||index>end) return;
+	private static void update(int start, int end, int node, int idx, long dif) {
+		if(idx<start || idx>end) return;
 		tree[node]+=dif;
 		if(start==end) return;
-		int mid = (start+end)/2;
-		update(start,mid,node*2,index,dif);
-		update(mid+1,end,node*2+1,index,dif);
+		int mid  = (start+end)/2;
+		update(start,mid,node*2,idx,dif);
+		update(mid+1,end,node*2+1,idx,dif);
 	}
+	private static Long init(int start, int end, int node) {
+		if(start==end) return tree[node] = nums[start];
+		int mid = (start+end)/2;
+		return tree[node] = init(start,mid,node*2) + init(mid+1,end,node*2+1) ;
+	}
+
 }
