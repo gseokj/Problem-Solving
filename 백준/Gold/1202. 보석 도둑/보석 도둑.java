e6aefddb97d34408cca90ad4a4f3f3411dfbb00c
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2023-10-26 23:26:36 +0900

    [Gold II] Title: 보석 도둑, Time: 1452 ms, Memory: 137560 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,71 +1,59 @@
 import java.util.*;
-	import java.io.*;
-	
-	public class Main {
-		static int N, K;
-	
-		static class Gem implements Comparable<Gem> {
-			int m;
-			int v;
-	
-			public Gem(int m, int v) {
-				super();
-				this.m = m;
-				this.v = v;
-			}
-	
-			@Override
-			public int compareTo(Gem o) {
-				
-					return this.m - o.m;
-			}
-	
-			@Override
-			public String toString() {
-				return "Gem [m=" + m + ", v=" + v + "]";
-			}
-	
+import java.io.*;
+
+public class Main {
+	static int N, K;
+
+	static class Gem implements Comparable<Gem> {
+		int m;
+		int v;
+
+		public Gem(int m, int v) {
+			super();
+			this.m = m;
+			this.v = v;
 		}
-	
-		static PriorityQueue<Integer> pq;
-		static List<Gem> gems = new ArrayList();
-		static List<Integer> bag;
-	
-		public static void main(String[] args) throws Exception {
-		//	System.setIn(new FileInputStream("src/input.txt"));
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			K = Integer.parseInt(st.nextToken());
-			pq = new PriorityQueue(Collections.reverseOrder());
-			bag = new ArrayList<Integer>();
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				int m = Integer.parseInt(st.nextToken());
-				int v = Integer.parseInt(st.nextToken());
-				gems.add(new Gem(m, v));
-			}
-			Long res = 0L;
-			int max = 0;
-			Collections.sort(gems);
-			for (int i = 0; i < K; i++) {
-				int m = Integer.parseInt(br.readLine());
-				max = Math.max(max, m);
-				bag.add(m);
-			}
-			Collections.sort(bag);
-//			System.out.println(gems);
-	        for (int i = 0, j = 0; i < K; i++) {
-	            while (j < N && gems.get(j).m <= bag.get(i)) {
-	                pq.offer(gems.get(j).v);
-//	                System.out.println(i+" "+pq);
-	                j++;
-	            }
-	            if (!pq.isEmpty()) {
-	                res += pq.poll();
-	            }
-	        }
-	        System.out.println(res);
-			
+
+		@Override
+		public int compareTo(Gem o) {
+			return m - o.m;
 		}
+
+
 	}
+
+	static PriorityQueue<Integer> pq;
+	static PriorityQueue<Integer> bags;
+	static PriorityQueue<Gem> gems;
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("src/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		pq = new PriorityQueue(Collections.reverseOrder());
+		bags = new PriorityQueue<Integer>();
+		gems = new PriorityQueue();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int m = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			gems.add(new Gem(m, v));
+		}
+		for (int i = 0; i < K; i++) {
+			bags.offer(Integer.parseInt(br.readLine()));
+		}
+		Long res = 0L;
+		while (!bags.isEmpty()) {
+			int bag = bags.poll();
+			while (!gems.isEmpty() && gems.peek().m <= bag) {
+				pq.offer(gems.poll().v);
+			}
+			if (!pq.isEmpty()) {
+				res += pq.poll();
+			}
+		}
+		System.out.println(res);
+	}
+}
