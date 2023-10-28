24793aa961ec93f3bb5269637d758d2568bbdc7e
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2023-10-28 11:03:30 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 128 ms, Memory: 52,052 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/README.md
+++ SWEA/D2/1288. 새로운 불면증 치료법/README.md
@@ -6,6 +6,10 @@
 
 메모리: 52,052 KB, 시간: 128 ms, 코드길이: 272 Bytes
 
+### 제출 일자
+
+2023-04-23 16:57
+
 
 
 > 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -1,13 +1,32 @@
+import java.util.*;
+import java.io.*;
 
-for i in range(1,int(input())+1):
-    lis = []
-    n = int(input())
-    cnt = 0
-    while len(lis) < 10 :
-        cnt+=1
-        test = n * cnt
-        for j in str(test)  :
-            if int(j) not in lis :
-                lis.append(int(j))
-
-    print(f"#{i} {n*cnt}")
+public class Solution {
+	static int T,N;
+	static int nums;
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("src/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		int res = (1<<10)-1;
+		
+		for (int i = 1; i < T+1; i++) {
+			int cnt = 0;
+			nums = 0;
+			char b= 'a';
+			N = Integer.parseInt(br.readLine());
+			int n = N;
+			while(nums!=res) {
+				cnt++;
+				char[] sheep = String.valueOf(n).toCharArray();
+				for (int j = 0; j < sheep.length; j++) {
+					
+					nums = nums|1<<(int)sheep[j]-'0';
+				}
+				
+				n+=N;
+			}
+			System.out.println("#"+i+" "+N*cnt);
+		}
+	}
+}
